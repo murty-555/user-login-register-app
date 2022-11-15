e6f36f7752b32485d3fb466592e8e543c931b98c
Bhargavi-yarlagadda-22 <bhargaviyarlagadda98@gmail.com>
2022-11-15 18:10:01 +0530

    added register

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./Header";
+import Registration from "./Registration";
 
 function App() {
   return (
     <div>
       <Header />
+      <Registration/>
     </div>
   );
 }
